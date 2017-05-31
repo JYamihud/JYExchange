0c474d0b62cef97c5aee898923034b8f473b5a32
JYamihud <blenderdumbass@gmail.com>
2017-06-01 00:40:07 +0300

    Update jyexchange.py

--- jyexchange.py
+++ jyexchange.py
@@ -13,7 +13,7 @@ import datetime
 import pango
 import zipfile
 
-VERSION = 0.21 # A software version for the updater
+VERSION = 0.3 # A software version for the updater
 
 def main_quit(widget):
     gtk.main_quit()
